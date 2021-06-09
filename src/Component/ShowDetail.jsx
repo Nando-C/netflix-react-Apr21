9b53021d6bd64d0345d8113ff6008418f771de00
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-06-09 16:42:13 +0100

    Update ShowDetail.jsx

--- src/Component/ShowDetail.jsx
+++ src/Component/ShowDetail.jsx
@@ -25,10 +25,10 @@ const ShowDetail = ({match}) => {
                 ? <h2>404 - MOVIE NOT FOUND</h2>
                 : (
                     <Row>
-                        <Col md={6}>
+                        <Col md={4}>
                             <img src={selectedMovie.Poster} alt="pic" className='m-5'/>
                         </Col>
-                        <Col md={6}>
+                        <Col md={8} className='p-4'>>
                             <h2 className='my-2'>Title: <br /> {selectedMovie.Title}</h2>
                             <h2 className='my-1'>Year:  {selectedMovie.Year}</h2>
                             <h2 className='my-1'>Rated: {selectedMovie.Rated}</h2>
